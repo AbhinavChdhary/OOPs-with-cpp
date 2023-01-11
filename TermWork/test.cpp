de3ba1226c5aca00f9d465ac084ee71e649cfc7b
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-01-11 09:33:35 +0530

    update

--- TermWork/test.cpp
+++ TermWork/test.cpp
@@ -1,33 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class A{
+    public:
+    A(){cout<<"A called\n";}
+    ~A(){cout<<"A destroyed\n";}
+};
+class B: public A{
+    public:
+    B(){cout<<"B called\n";}
+    ~B(){cout<<"B destroyed\n";}
+};
+class C: public B{
+    public:
+    C(){cout<<"C called\n";}
+    ~C(){cout<<"C destroyed\n";}
+};
 int main(){
-    string st;
-
-    cout<<"Enter: ";
-    getline(cin>>ws, st);
-
-    for (size_t i = 0; i < st.length(); i++)
-    {
-        swap(st.at(i), st.at(st.length()-1-i));
-    }
-    cout<<"reversed word: "<<st<<endl;
-    //to count duplicates
-    string copy = st;
-    for (size_t i = 0; i < copy.length(); i++)
-    {
-        char ch = copy.at(i);
-        int count = 1;
-        for(size_t j = i+1; j < copy.length(); j++){
-            if(copy.at(j)==ch && ch!='\0'){
-                cout<<copy.at(j);
-                copy.at(j) = '\0';
-                count++;
-            }
-        }
-        if(ch!='\0')
-            cout<<"count of "<<ch<<" = "<<count<<endl;
-    }
+    C ob1;
+    cout<<"2\n";
+    C ob2;
     
     return 0;
 }
